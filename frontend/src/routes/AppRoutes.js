4ad637c6d0f41cec64fa50e793ef110604a069a9
TrungTien2004_@UTH <2251120447@ut.edu.vn>
2025-01-03 13:33:22 +0700

    update

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -5,17 +5,33 @@ import ProductDetail from "../page/ProductDetail";
 import TableUsers from "../components/TableUsers";
 import Register from "../page/register";
 import PrivateRoute from "./PrivatRoute";
+import Admin from "../Admin/pages/AdminHome";
 import About from "../page/about";
 import NotFound from "./NotFound";
+import UserManagement from "../Admin/pages/UserManagement";
+import ShowtimeManagement from "../Admin/pages/ShowtimeManagement";
+import BookingManagement from "../Admin/pages/BookingManagement";
+
 const AppRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route path="/Admin" element={<Admin />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
         <Route path="/ProductDetail" element={<ProductDetail />} />
+        <Route path="/admin/user-management" element={<UserManagement />} />
+        <Route
+          path="/admin/showtime-management"
+          element={<ShowtimeManagement />}
+        />
+        <Route
+          path="/admin/booking-management"
+          element={<BookingManagement />}
+        />
 
         <Route
           path="/users"
